9492128fe8f479ae7dbe8bb2ae42ca3fdf29648c
StevenR123 <stevenreninga2@gmail.com>
2024-07-28 16:15:50 -0400

    Quartz sync: Jul 28, 2024, 4:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "stevenr123.github.io/Istina-Wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
