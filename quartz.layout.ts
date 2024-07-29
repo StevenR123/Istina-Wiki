51feed8873dece22830b52af16336d9e2d8a5681
StevenR123 <stevenreninga2@gmail.com>
2024-07-29 11:18:34 -0400

    Quartz sync: Jul 29, 2024, 11:18 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'StevenR123/Istina-Wiki',
+        // from data-repo-id
+        repoId: 'R_kgDOMcRRVA',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMcRRVM4ChQYf',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
     },
@@ -31,7 +45,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.MobileOnly(Component.Spacer()),
     Component.MobileOnly(Component.Explorer()),
   ],
 }
